6407ec2b9df53b0c54fed09b3f377adf427668ae
Joshua Gilman <joshuagilman@gmail.com>
2022-03-03 15:21:10 -0800

    fix: fixes missing flag on checksum generation

--- checksum.sh
+++ checksum.sh
@@ -7,7 +7,7 @@
 #/ Calculates the checksum of setup.sh and saves it to setup.sh.sha256
 #/
 
-if ! shasum -a 256 setup.sh.sha256 &>/dev/null; then
+if ! shasum -a 256 -c setup.sh.sha256 &>/dev/null; then
 	shasum -a 256 setup.sh >setup.sh.sha256
 	exit 1
 fi
