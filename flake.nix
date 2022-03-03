3a657afde5af8aedee9f76fceb6fb31df3e26427
Joshua Gilman <joshuagilman@gmail.com>
2022-03-03 09:31:25 -0800

    build: adds nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix-src.url = github:jmgilman/poetry2nix;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix-src }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ poetry2nix-src.overlay ];
+        };
+
+        poetry = pkgs.poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          python = pkgs.python310;
+          preferWheels = true;
+          overrides = [
+            pkgs.poetry2nix.defaultPoetryOverrides
+          ];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          packages = [poetry pkgs.nodePackages.markdownlint-cli];
+        };
+      }
+    );
+}
